464b41af5394743109272d221b2b86ad38fd8e13
bioguo <guokai8@gmail.com>
2018-08-07 01:27:28 -0500

    Update fastqc.rb

--- Formula/fastqc.rb
+++ Formula/fastqc.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Fastqc < Formula
   homepage 'http://www.bioinformatics.babraham.ac.uk/projects/fastqc/'
   url 'http://www.bioinformatics.babraham.ac.uk/projects/fastqc/fastqc_v0.11.2.zip'
-  sha1 'd4dc1b903de35aa4de8e995c4974e0869db99dda'
+  sha256 'f362b141e696d67442fa16c3a71118936e0b14a0343bb88735973a920cbaef09'
 
   def install
     chmod 0755, 'fastqc'
