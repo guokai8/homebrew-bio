0ecd0d9012e5ca0e83d7fbabe19f7c64076c617b
bioguo <guokai8@gmail.com>
2018-08-07 00:48:20 -0500

    Create brew-cask.rb

---
+++ Formula/brew-cask.rb
@@ -0,0 +1,8 @@
+require 'formula'
+
+class BrewCask < Formula
+  homepage 'https://github.com/yoshiko-pg/homebrew-cask'
+  url 'https://github.com/yoshiko-pg/homebrew-cask.git'
+  def install
+  end
+end
