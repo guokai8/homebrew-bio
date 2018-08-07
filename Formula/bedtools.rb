7c7c63cab4277d11ea0905d8b3e5febfecd2d119
bioguo <guokai8@gmail.com>
2018-08-07 01:21:14 -0500

    Update bedtools.rb

--- Formula/bedtools.rb
+++ Formula/bedtools.rb
@@ -5,7 +5,7 @@ class Bedtools < Formula
   #doi '10.1093/bioinformatics/btq033'
   #tag "bioinformatics"
   url "https://github.com/arq5x/bedtools2/releases/download/v2.21.0/bedtools-2.21.0.tar.gz"
-  sha1 "ceeb1871bec66e90f68a1bef24d82c06669c47d8"
+  sha256 "a1247308737d13087e422914ac5ca696741d73b3f9543432d2bf6dbc32068ab9"
   head 'https://github.com/arq5x/bedtools2.git'
 
   def install
