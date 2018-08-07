7d89cdd68645dc9b568810ddc8665281732a7266
bioguo <guokai8@gmail.com>
2018-08-07 01:16:34 -0500

    Update bamtools.rb

--- Formula/bamtools.rb
+++ Formula/bamtools.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Bamtools < Formula
   homepage 'https://github.com/pezmaster31/bamtools'
   url 'https://github.com/pezmaster31/bamtools/archive/v2.3.0.tar.gz'
-  sha1 '397d595b373ccc11503856d7e2804833aa8ea811'
+  sha256 '288046e6d5d41afdc5fce8608c5641cf2b8e670644587c1315b90bbe92f039af'
   head 'https://github.com/pezmaster31/bamtools.git'
 
   depends_on 'cmake' => :build
@@ -12,7 +12,7 @@ class Bamtools < Formula
     # Install libbamtools in /usr/local/lib.
     # https://github.com/pezmaster31/bamtools/pull/82
     url "https://github.com/sjackman/bamtools/commit/3b6b89d.diff"
-    sha1 "89f659243dac265684705b485b5580e7cac559f7"
+    sha256 "de9438e932884a3f941a683984bed32b2eaca5c5483642c5a2253074f4a559ff"
   end
 
   def install
